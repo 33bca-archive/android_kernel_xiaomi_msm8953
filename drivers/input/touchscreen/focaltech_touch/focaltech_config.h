ccac9e15d453fa9c41e258f2e61b2aa479840ec8
Offain <fabian.leutenegger@bluewin.ch>
2019-07-30 13:44:40 +0200

    drivers: input: touchscreen: Use the stock value to define the type
    Change-Id: I056e18ffaf6ff3013fdfa94fb72dc98150961418

--- drivers/input/touchscreen/focaltech_touch/focaltech_config.h
+++ drivers/input/touchscreen/focaltech_touch/focaltech_config.h
@@ -46,7 +46,7 @@
 #define _FT5436     0x54360002
 #define _FT5526     0x55260002
 #define _FT5526I    0x5526B002
-#define _FT5446     0x54460402
+#define _FT5446     0x54460002
 #define _FT5346     0x53460002
 #define _FT5446I    0x5446B002
 #define _FT5346I    0x5346B002
